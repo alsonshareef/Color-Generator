b2784ee82ea5e236cd3d7f53397e0a87836d9abb
Alson Shareef <alson.shareef@gmail.com>
2018-08-05 21:03:07 +0800

    added text content to be gradient color

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ let color1 = document.querySelector(".color1");
 let color2 = document.querySelector(".color2");
 let body = document.getElementById("gradient");
 
-color1.addEventListener("input", function(){
+function setGradient (){
     body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
-});
+    h3.textContent = body.style.background + ";";
+}
 
-color2.addEventListener("input", function(){
-    body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
-});
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
